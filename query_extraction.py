298cfab7cdc1bd2fdb89a559b750233ceafdbf4b
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-02 12:05:06 +0530

    Update query_extraction.py

--- query_extraction.py
+++ query_extraction.py
@@ -9,31 +9,18 @@ from transformers import (
     BitsAndBytesConfig,
     pipeline
 )
-# from accelerate import Accelerator
-# from accelerate import load_checkpoint_and_dispatch
 import json
 
 with open('metadata.json') as f:
     d = json.load(f)
 
-
-tokenizer, model = build_model(model_repo = CFG.model_name)
+tokenizer, model = build_model(model_repo=CFG.model_name)
 terminators = [
     tokenizer.eos_token_id,
     tokenizer.bos_token_id
 ]
 
-# save_directory= "./model"
-# # accelerator = Accelerator()
-
-# # accelerator.save_model(model=model, save_directory=save_directory,max_shard_size="500MB")
-# device_map={'':'cpu'}
-# model = load_checkpoint_and_dispatch(
-# model, checkpoint="./model/", device_map=device_map, no_split_module_classes=['Block']
-# )
-# tokenizer, model = build_model(model_repo = CFG.model_name)
-
-def generate_md(Question,query):
+def generate_md(Question, query):
     messages = [{"role": "user", "content": f"{Question}{query}"}]
     inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
     outputs = model.generate(inputs, max_new_tokens=150)
@@ -44,24 +31,24 @@ def generate_md(Question,query):
     match = re.search(pattern, text)
     if match:
         output_list = match.group(0)
-        return(ast.literal_eval(output_list))
+        return ast.literal_eval(output_list)
     else:
         print("No match found")
-        return("[]")
+        return "[]"
 
 def model_pipeline():
     pipe = pipeline(
-        task = "text-generation",
-        model = model,
-        tokenizer = tokenizer,
-        eos_token_id = terminators,
-        do_sample = True,
-        max_length = CFG.max_len,
-        max_new_tokens = CFG.max_new_tokens,
-        temperature = CFG.temperature,
-        top_p = CFG.top_p,
-        repetition_penalty = CFG.repetition_penalty,
+        task="text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        eos_token_id=terminators,
+        do_sample=True,
+        max_length=CFG.max_len,
+        max_new_tokens=CFG.max_new_tokens,
+        temperature=CFG.temperature,
+        top_p=CFG.top_p,
+        repetition_penalty=CFG.repetition_penalty,
     )
 
-    llm = HuggingFacePipeline(pipeline = pipe)
+    llm = HuggingFacePipeline(pipeline=pipe)
     return llm
