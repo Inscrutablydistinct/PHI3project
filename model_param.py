1becf73fa26c1beb4609f63e55609b2c507493de
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-04 13:30:32 +0530

    Update model_param.py

--- model_param.py
+++ model_param.py
@@ -9,7 +9,7 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 class CFG:
     DEBUG = False
     model_name = 'microsoft/Phi-3-mini-128k-instruct'
-    temperature = 0.4
+    temperature = 0.0
     top_p = 0.90
     repetition_penalty = 1.15
     max_len = 256
