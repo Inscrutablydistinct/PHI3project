16d87bd31ddec8284bc088e0b2190d1d2f85af94
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-04 16:32:18 +0530

    Update model_param.py

--- model_param.py
+++ model_param.py
@@ -15,7 +15,7 @@ class CFG:
     max_len = 200
     max_new_tokens = 200
     split_chunk_size = 512
-    split_overlap = 400
+    split_overlap = 100
     embeddings_model_repo = 'BAAI/bge-m3'
     k = 6
     PDFs_path = './New Folder With Items'
