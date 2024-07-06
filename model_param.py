2d2114af02d4beb18268e09c0d185a31ec685029
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-06 14:27:15 +0530

    Update model_param.py

--- model_param.py
+++ model_param.py
@@ -12,8 +12,8 @@ class CFG:
     temperature = 0.4
     top_p = 0.9
     repetition_penalty = 1.15
-    max_len = 200
-    max_new_tokens = 200
+    max_len = 300
+    max_new_tokens = 300
     split_chunk_size = 512
     split_overlap = 100
     k = 6
